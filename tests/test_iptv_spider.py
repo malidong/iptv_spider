454bb9e3a7dbe7f5011454548293905600a8b646
jcv-lidong.ma <lidong.ma@japancv.co.jp>
2024-12-16 00:56:16 +0900

    Fix unittests.

--- tests/test_iptv_spider.py
+++ tests/test_iptv_spider.py
@@ -11,7 +11,7 @@ class TestM3U8(unittest.TestCase):
     __slots__ = "m"
 
     def setUp(self):
-        self.m: M3U8 = M3U8(path="https://live.iptv365.org/live.m3u", regex_filter="*")
+        self.m: M3U8 = M3U8(path="https://live.iptv365.org/live.m3u", regex_filter=r".*")
 
     def test_download_m3u8_file(self):
         trial_url = "https://live.iptv365.org/live.m3u"
